0568aceb8e53ae6ebab4b3e9a14894cd2c9bd33a
miet001 <m.ieti001@gmail.com>
2015-09-18 15:39:32 +1200

    readded MediaFormatter class

---
+++ src/player/MediaFormatter.java
@@ -0,0 +1,27 @@
+package player;
+
+import java.net.URI;
+
+import javafx.scene.media.Media;
+
+public class MediaFormatter {
+	
+	static int MP4 = 1;
+
+	public static Media checkMediaFormat(Media media){
+		
+		//TODO: attempt to check extension
+		//if extension is mp4 or flv
+		
+		return null;
+	}
+	
+	public static URI transformMedia(URI mediaPath, int mediaType){
+		
+		//converts the file at the URI to an acceptable file type
+		
+		return null;
+	}
+	
+	
+}
