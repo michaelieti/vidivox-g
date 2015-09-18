353fd3c9e2c6c7aba6f14293b792545fbd9d722d
miet001 <m.ieti001@gmail.com>
2015-09-18 15:51:44 +1200

    Updated the interface for the MediaFormatter class

--- src/player/MediaFormatter.java
+++ src/player/MediaFormatter.java
@@ -8,14 +8,27 @@ public class MediaFormatter {
 	
 	static int MP4 = 1;
 
-	public static Media checkMediaFormat(Media media){
+	/**
+	 * Checks if a media located at a URI object is acceptable to be played
+	 * by JavaFX's MediaPlayer and its related components.
+	 * @param mediaPath - a URI object pointing to the media to be transformed
+	 * @return - boolean on whether it is MP4 or FLV format
+	 */
+	public static boolean isFormatAcceptable(URI mediaPath){
 		
 		//TODO: attempt to check extension
 		//if extension is mp4 or flv
 		
-		return null;
+		return false;
 	}
 	
+	
+	/**
+	 * Takes a media file at the specified URI and transforms it to a specified file.
+	 * @param mediaPath - a URI object to be transformed
+	 * @param mediaType - MediaFormatter.MP4 or MediaFormatter.FLV
+	 * @return URI object pointing to the reformatted file.
+	 */
 	public static URI transformMedia(URI mediaPath, int mediaType){
 		
 		//converts the file at the URI to an acceptable file type
