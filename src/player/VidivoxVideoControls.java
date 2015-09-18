9638592c7b199993d0b856f76484fe70d56eb1b1
adav194 <adav194@xen-rlimage.ece.auckland.ac.nz>
2015-09-18 15:53:29 +1200

    Intermediate Commit, working on the video controls file

--- src/player/VidivoxVideoControls.java
+++ src/player/VidivoxVideoControls.java
@@ -4,13 +4,20 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class VidivoxVideoControls extends VBox {
 
 	private VidivoxLauncher launcher;
-	private Button playBtn, pauseBtn, stopBtn, skipBackBtn, skipFwdBtn;
+	private Button playBtn, pauseBtn, stopBtn, skipBackBtn, skipFwdBtn; //Video Playback
+	private Button speechBtn, subBtn, mp3Btn; //Video Editting
+	private Slider volumeBar;
+	
+	final private static double minVolume = 0.0;
+	final private static double maxVolume = 10.0;
+	final private static double defaultVolume = 5.0;
 	
 	public VidivoxVideoControls(VidivoxLauncher vl) {
 		super();
@@ -47,12 +54,15 @@ public class VidivoxVideoControls extends VBox {
 				launcher.rwdVideo();
 			}
 		});
-		
+		speechBtn = new Button("Spch");
+		speechBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+		});
 		//ADD IN TOP PANEL
 		HBox cp_top = new HBox();
 		cp_top.setAlignment(Pos.CENTER);
 		cp_top.setSpacing(10);
-		cp_top.getChildren().addAll(playBtn, pauseBtn, stopBtn);
+		cp_top.getChildren().addAll(skipBackBtn, stopBtn, playBtn, pauseBtn, skipFwdBtn);
 		
 		//ADD IN MIDDLE PANEL
 		HBox cp_mid = new HBox();
