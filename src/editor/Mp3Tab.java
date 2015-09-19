3e3360c157f4ba7565c2959180c54b070e9ed805
adav194 <adav194@xen-rlimage.ece.auckland.ac.nz>
2015-09-19 14:19:21 +1200

    Added a file path box to Mp3Tab

--- src/editor/Mp3Tab.java
+++ src/editor/Mp3Tab.java
@@ -2,6 +2,9 @@ package editor;
 
 import java.io.File;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.StringPropertyBase;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -11,6 +14,7 @@ import javafx.scene.control.TabPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
@@ -23,9 +27,10 @@ public class Mp3Tab extends BindableTab {
 	
 	private Text msg;
 	private Stage binder;
-	private Button browseBtn,selectBtn;
+	private Button browseBtn,okBtn;
 	private final FileChooser fc = new FileChooser();
 	private File userFile = null;
+	private StringProperty filePath = new SimpleStringProperty();
 	
 	public Mp3Tab(String title, String message) {
 		super(title);
@@ -33,30 +38,34 @@ public class Mp3Tab extends BindableTab {
 		//Initializing Button Event handlers
 		browseBtn = new Button("Browse");
 		browseBtn.setOnAction(new EventHandler<ActionEvent>(){
-			@Override
 			public void handle(ActionEvent event) {
 				//open up file chooser & get user selection as File object
 				userFile = fc.showOpenDialog(binder);
 				if (userFile != null){
 					//TODO: File now stored in userFile, ready to manipulate.
+					filePath.setValue(userFile.getPath());
 				}
 			}
 			
 		});
-		selectBtn = new Button("Select");
-		selectBtn.setOnAction(null);
-		
+		okBtn = new Button("Ok");
+		okBtn.setOnAction(null);
+		TextField currentFile = new TextField("");
+		filePath.setValue("");
+		currentFile.textProperty().bind(filePath);
 		GridPane mp3Pane = new GridPane();
 		mp3Pane.setGridLinesVisible(GRID_VISIBLE);
 		mp3Pane.setVgap(10);
 		mp3Pane.setHgap(10);
 		mp3Pane.setPadding(new Insets(10, 10, 10, 10));
-		mp3Pane.add(msg, 0, 0);
-		HBox mp3Butons = new HBox();
-		mp3Butons.setAlignment(Pos.CENTER);
-		mp3Butons.setSpacing(btnSpacing);
-		mp3Butons.getChildren().addAll(browseBtn, selectBtn);
-		mp3Pane.add(mp3Butons, 0, 1);
+		mp3Pane.add(msg, 0, 0, 2, 1);
+		HBox mp3Buttons = new HBox();
+		mp3Buttons.setAlignment(Pos.CENTER);
+		mp3Buttons.setSpacing(btnSpacing);
+		mp3Buttons.getChildren().add(okBtn);
+		mp3Pane.add(browseBtn, 0, 1);
+		mp3Pane.add(currentFile, 1, 1);
+		mp3Pane.add(mp3Buttons, 0, 2, 2, 1);
 		
 		this.setContent(mp3Pane);
 		
@@ -67,6 +76,8 @@ public class Mp3Tab extends BindableTab {
 		binder = toBindTo;
 		return;
 	}
-
-
+	
+	public File getFile() {
+		return userFile;
+	}
 }
