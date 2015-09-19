0ad1bd7365a6c8dd68c3cae447874c6750958106
adav194 <adav194@xen-rlimage.ece.auckland.ac.nz>
2015-09-19 13:26:28 +1200

    Updated ./compileme.sh to accomodate new package structure

--- compileme.sh
+++ compileme.sh
@@ -4,5 +4,6 @@
 # be run by runme.sh
 
 cd src
-/usr/lib/jvm/jdk8/bin/javac -cp . player/*.java
-/usr/lib/jvm/jdk8/bin/jar cvfm vidivox.jar ../manifest.txt player/*.class
+/usr/lib/jvm/jdk8/bin/javac -cp . `find | grep .java$`
+/usr/lib/jvm/jdk8/bin/jar cvfm vidivox.jar ../manifest.txt `find | grep .class$`
+rm -f `find | grep .class$`
