157fc00654871890ffba21b29994079a6e861384
Michael <m.ieti001@gmail.com>
2015-09-21 02:29:51 +1200

    rudimentary implementation of convertToMP4 in MediaConverter.java complete

--- src/editor/MediaConverter.java
+++ src/editor/MediaConverter.java
@@ -7,6 +7,11 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.ExecutionException;
+
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.scene.media.Media;
 
 /**
  * The purpose of this class will be to provide all the necessary functionality to edit a video.
@@ -51,13 +56,54 @@ public class MediaConverter {
 		return new File(System.getProperty("user.dir") + "/.temp/speech.wav");
 	}
 	
-	public static void convertToMP4(String inputPath, String outputPath) {
+	/**
+	 * Version 0.1 - not yet tested.
+	 * Converts a file (given via a path string) to an mp4 that will be compatible with
+	 * vidivox. 
+	 * @param inputPath - the complete absolute input path, including the extension.
+	 * @param outputPath - the absolute output path, including the name of the file. The extension
+	 * is automatically assigned as .mp4 and does not need to be included.
+	 * @throws ExecutionException 
+	 * @throws InterruptedException 
+	 */
+	public static Media convertToMP4(Path inputPath, Path outputPath) throws InterruptedException, ExecutionException {
 		//ProcessBuilder builds the process below
 		//ffmpeg -i <inputPath> -f mp4 -strict -2 -c:v libx264 -t 0 <outputPath>.mp4
-		String command = buildFFMPEGCommand(inputPath, outputPath);
-		
+		//TODO: check that Path.toString() works properly here
+		String command = buildFFMPEGCommand(inputPath.toString(), outputPath.toString());
 		ProcessBuilder pb = new ProcessBuilder("/bin/bash", "-c", command);
+		//TODO: test this shit out
+		Task<Media> task = new Task<Media>(){
+			@Override
+			protected Media call() throws Exception {
+				Process p = pb.start();
+				//TODO: check isCancelled() so that it can be cancelled
+				Platform.runLater(new Runnable(){
+					@Override
+					public void run() {
+						//create a new Dialog saying some shit like "processing..." etc
+						System.out.println("Processing conversion...");
+					}
+				});	//end runnable
+				p.waitFor();
+				Media media = new Media(outputPath+".mp4");
+				return media;
+			}	//end call
+		};	//end task implementation
+		return task.get();
 	}
+	/**
+	 * A utility method for convertToMP4(). Not for public use.
+	 */
+	private static String buildFFMPEGCommand(String in, String out){
+		StringBuilder sb = new StringBuilder("ffmpeg -i ");
+		sb.append(in);
+		sb.append(" -f mp4 -strict -2 -c:v libx264 -t 0 ");
+		sb.append(out);
+		sb.append(".mp4");
+		return sb.toString();
+	}
+	
 	public static Double HowLongToGo() {
 		return null;
 		//TODO: Parse the output of ffmpeg to see how long it has to go
