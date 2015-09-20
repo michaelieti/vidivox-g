69475b11a37535b75580d713821c1cbeb57ff8ae
Michael <m.ieti001@gmail.com>
2015-09-20 12:55:32 +1200

    removed placeholder functions from Launcher. Hooked up the volume slider, added in some comments for FileControls.

--- src/player/VidivoxVideoControls.java
+++ src/player/VidivoxVideoControls.java
@@ -1,5 +1,7 @@
 package player;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.beans.property.ObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -70,7 +72,21 @@ public class VidivoxVideoControls extends HBox {
 		//Initializing the Volume Slider
 
 		volumeBar = new Slider(minVolume, maxVolume, defaultVolume);
-
+		//the volumeBar's valueProperty registers a listener, that is notified when
+		//	that property is invalidated. Invalidation is caused by e.g. the value
+		//	property not being equal to the slider's position
+		volumeBar.valueProperty().addListener(new InvalidationListener() {
+			
+			@Override
+			public void invalidated(Observable observable) {
+				if (volumeBar.isValueChanging()){
+					double currentVol = volumeBar.getValue() / 10.0;
+					mediaView.getMediaPlayer().setVolume(currentVol);
+				}
+			}
+		});
+		
+		
 		this.setAlignment(Pos.CENTER);
 		this.setSpacing(10);
 		this.getChildren().addAll(skipBackBtn, stopBtn, playBtn, skipFwdBtn, volumeBar);
