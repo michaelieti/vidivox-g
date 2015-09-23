1b812d1ea97b43c63c5ebb7b790f574a83686857
Michael <m.ieti001@gmail.com>
2015-09-23 16:19:13 +1200

    added in static names for min/max duration

--- src/player/VidivoxMedia.java
+++ src/player/VidivoxMedia.java
@@ -1,8 +1,11 @@
 package player;
 
 import javafx.scene.layout.VBox;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.scene.control.Slider;
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 
 /**
  * The panel holding the media player.
@@ -12,14 +15,20 @@ public class VidivoxMedia extends VBox {
 	private MediaView mv;
 	private Slider mediaTimeline;
 	
+	private static final double MINTIME = 0.0;
+	private static final double MAXTIME = 100.0;
+	private static final double STARTTIME = 0.0;
+	
 	public VidivoxMedia() {
 		super();
 		mv = new MediaView();
 		mv.setFitHeight(500);
 		mv.setFitWidth(800);
-		mediaTimeline = new Slider(0.0,1.0,0.0);  //TODO: subclass this timeline. tie timeline to media file so it updates accordingly.
+		mediaTimeline = new Slider(MINTIME, MAXTIME, STARTTIME);  //TODO: subclass this timeline. tie timeline to media file so it updates accordingly.
 		this.getChildren().addAll(mv,mediaTimeline);
 	}
+	
+	
 	
 	public MediaView getMediaView() {
 		return mv;
