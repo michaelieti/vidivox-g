9389fa09fda6add9bf6f17eaee0d6e4891ab3dfd
Michael <m.ieti001@gmail.com>
2015-09-24 16:41:13 +1200

    started on overlay button work for speech tab in editor panel

--- src/editor/SpeechTab.java
+++ src/editor/SpeechTab.java
@@ -18,31 +18,30 @@ import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-
 public class SpeechTab extends BindableTab {
-	
+
 	final private static int btnSpacing = 20;
-	
+
 	private Text msg;
 	private TextArea userField;
 	private Button playBtn, saveBtn, overlayBtn;
 	private FileChooser f;
 	private Stage stage;
-	
-	public SpeechTab(MediaView mv,String title, String message) {
+
+	public SpeechTab(MediaView mv, String title, String message) {
 		super(mv, title);
 		msg = new Text(message);
 		userField = new TextArea();
 		f = new FileChooser();
 		f.setTitle("Save");
-		//Initializing Button Event handlers
+		// Initializing Button Event handlers
 		playBtn = new Button("Play");
 		playBtn.setOnAction(new EventHandler<ActionEvent>() {
-			
+
 			public void handle(ActionEvent arg0) {
 				playSpeech();
 			}
-			
+
 		});
 		saveBtn = new Button("Save Speech");
 		saveBtn.setOnAction(new EventHandler<ActionEvent>() {
@@ -56,11 +55,23 @@ public class SpeechTab extends BindableTab {
 					textToSpeech(userField.getText(), file.getAbsolutePath());
 				}
 			}
-			
+
 		});
 		overlayBtn = new Button("Overlay");
-		overlayBtn.setOnAction(null);
-		
+		overlayBtn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent arg) {
+				// make temp wav file
+				// call mp3 to wav
+				// call overlay metho0d in mp3
+				String msg = userField.getText();
+				String path = "~/temp/wav";
+				textToSpeech(msg, path);
+			}
+
+		});
+
 		GridPane speechPane = new GridPane();
 		speechPane.setGridLinesVisible(player.VidivoxLauncher.GRID_IS_VISIBLE);
 		speechPane.setVgap(10);
@@ -73,11 +84,11 @@ public class SpeechTab extends BindableTab {
 		speechBtns.setSpacing(btnSpacing);
 		speechBtns.getChildren().addAll(playBtn, saveBtn, overlayBtn);
 		speechPane.add(speechBtns, 0, 4, 3, 1);
-		
+
 		this.setContent(speechPane);
-		
+
 	}
-	
+
 	public void setBind(Stage toBindTo) {
 		msg.wrappingWidthProperty().bind(toBindTo.widthProperty().subtract(20));
 		return;
@@ -92,7 +103,7 @@ public class SpeechTab extends BindableTab {
 	private void playSpeech() {
 		System.out.println(userField.getText());
 		String expansion = "`echo \"" + userField.getText() + "\" | festival --tts`";
-		String[] cmd = {"bash" , "-c", expansion};
+		String[] cmd = { "bash", "-c", expansion };
 		ProcessBuilder build = new ProcessBuilder(cmd);
 		try {
 			build.start();
@@ -101,23 +112,25 @@ public class SpeechTab extends BindableTab {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@Override
 	public void stageMedia() {
 		String path = stagedMedia.getFile().getAbsolutePath();
 		textToSpeech(userField.getText(), path);
 	}
-	
+
 	/**
-	 * Convenience method which saves a particular string as an audio file
-	 * at a given path. Not for public use
+	 * Convenience method which saves a particular string as an audio file at a
+	 * given path. Not for public use
+	 * 
 	 * @param msg
 	 * @param path
 	 */
 	private void textToSpeech(String msg, String path) {
-		//TODO: Implement Concurrency
+		// TODO: Implement Concurrency
+		System.out.println("Saving to "+path);
 		String expansion = "`echo " + msg + " | text2wave > " + path + "`";
-		String[] cmd = {"bash", "-c", expansion};
+		String[] cmd = { "bash", "-c", expansion };
 		ProcessBuilder build = new ProcessBuilder(cmd);
 		try {
 			build.start();
@@ -129,14 +142,12 @@ public class SpeechTab extends BindableTab {
 	@Override
 	public void publishStage(StagedMedia media) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void initStagedMedia() {
 		stagedMedia = new StagedAudio(StagedAudio.MediaTypes.WAV);
 	}
-	
 
 }
-
