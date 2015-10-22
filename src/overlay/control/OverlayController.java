ad8e01558178d991e51f8acc4c8a08b7f1c0c689
Michael <m.ieti001@gmail.com>
2015-10-22 17:01:41 +1300

    completed implementation of controller interface and overlay type

---
+++ src/overlay/control/OverlayController.java
@@ -0,0 +1,47 @@
+package overlay.control;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import overlay.Commentary;
+
+
+/**
+ * Handles logical type operations for the overlay functionality.
+ * Does not attempt to affect the view of the overlay panel.
+ * @author michael
+ *
+ */
+public class OverlayController implements OverlayControllable {
+
+
+	private static OverlayController singletonObject;
+	//the list of commentaries
+	final private ObservableList<Commentary> commentaryList = FXCollections.observableArrayList();
+	
+	@Override
+	public void filterList(OverlayType olType) {
+		// TODO Auto-generated method stub
+		// sets filter off or onto a selected overlay type
+	}
+
+	@Override
+	public void editSelected() {
+		//TODO: gets the selection model, 
+		// obtains the selected comment, 
+		// and passes it to the editor controller class.
+	}
+
+	@Override
+	public void deleteSelected() {
+		// TODO: gets the selection model,
+		// obtains the selected comment
+		// deletes it from the list + reupdates the table.
+	}
+
+	@Override
+	public void commitOverlay() {
+		// TODO: creates a new video, overlaid with the commentary.
+
+	}
+
+}
